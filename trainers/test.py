1a42d008207aec60e820d82bfd34bca2a0f556a3
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2024-06-10 16:00:57 +0200

    Fix test mode by not providing `ckpt_path`, since weights are loaded as part of the model init.

--- trainers/test.py
+++ trainers/test.py
@@ -38,4 +38,6 @@ def test(hparams, wandb_logger=None):
   model = Evaluator(hparams)
   model.freeze()
   trainer = Trainer.from_argparse_args(hparams, gpus=1, logger=wandb_logger)
-  trainer.test(model, test_loader, ckpt_path=hparams.checkpoint)
+  # Don't need to provide `ckpt_path` when calling `trainer.test` since weights are already loaded
+  # when instantiating the model in the `Evaluator(hparams)` call
+  trainer.test(model, test_loader)
